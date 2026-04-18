7ad4f4c917745ae234702553efa6422da9f53424
dhanrajk046 <dhanrajk046@gmail.com>
2026-04-18 22:29:28 +0530

    add Procfile and final fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,10 @@ import os
 from dotenv import load_dotenv
 from datetime import timedelta
 
+import dj_database_url
+# import os
+
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -84,17 +88,21 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///db.sqlite3"
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
